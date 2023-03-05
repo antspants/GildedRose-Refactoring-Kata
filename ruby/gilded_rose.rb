9c5b9acbe0b51a8e49a12469870fcd7714534afa
Anthony Gardner <ants@fastmail.fm>
2023-03-05 13:34:36 +0000

    Call next in each loop if sulfuras as nothing needs to be done for this item

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,6 +6,8 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      next if item.name == "Sulfuras, Hand of Ragnaros"
+
       if item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert"
         if item.quality < 50
           item.quality = item.quality + 1
@@ -20,14 +22,12 @@ class GildedRose
         end
       else
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+          item.quality = item.quality - 1
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+
+      item.sell_in = item.sell_in - 1
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
