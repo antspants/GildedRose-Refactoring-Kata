23b80547d9fd16adf7cf30f367f2351b5116cc6a
Anthony Gardner <ants@fastmail.fm>
2023-03-05 14:30:24 +0000

    Remove checks for < 50 in aged brie and then do the check at the end of the statement and either return the new item quality value or return the max item quality value if we have gone too high

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -10,10 +10,16 @@ class GildedRose
       when "Sulfuras, Hand of Ragnaros"
         ## do nothing
       when "Aged Brie"
-        item.quality += 1 if item.quality < 50
+        ## item.quality += 1 if item.quality < 50
+        increase_by = 1
+
         item.sell_in = item.sell_in - 1
 
-        item.quality += 1 if item.sell_in < 0 && item.quality < 50
+        ## item.quality += 1 if item.sell_in < 0 && item.quality < 50
+        increase_by += 1 if item.sell_in < 0
+
+        item.quality = (item.quality + increase_by <= 50) \
+          ? item.quality + increase_by : 50
 
       when "Backstage passes to a TAFKAL80ETC concert"
         if item.quality < 50
