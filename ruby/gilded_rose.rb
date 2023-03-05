caf2f03c4a3c6f51570da67ebf5d2592e59d1df7
Anthony Gardner <ants@fastmail.fm>
2023-03-05 13:31:46 +0000

    Combine if condition into parent condition for checking quality < 50 for backstage pass sell_in

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -10,15 +10,11 @@ class GildedRose
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+            if item.sell_in < 11 && item.quality < 50
+              item.quality = item.quality + 1
             end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+            if item.sell_in < 6 && item.quality < 50
+              item.quality = item.quality + 1
             end
           end
         end
