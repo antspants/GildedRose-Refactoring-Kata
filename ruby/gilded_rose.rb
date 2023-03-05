9f7facc4c20a196d80c7c788496ba14c272985d6
Anthony Gardner <ants@fastmail.fm>
2023-03-05 14:06:12 +0000

    Move Backstage passes into case statement and by default Standard item has been refacted too

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -13,30 +13,30 @@ class GildedRose
         item.quality += 1 if item.quality < 50
         item.sell_in = item.sell_in - 1
 
-        item.quality += 1 if item.quality < 50 if item.sell_in < 0
-      else
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          if item.quality < 50
+        item.quality += 1 if item.sell_in < 0 && item.quality < 50
+
+      when "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality < 50
+          item.quality += 1
+
+          if item.sell_in < 11 && item.quality < 50
             item.quality += 1
-            if item.name == "Backstage passes to a TAFKAL80ETC concert"
-              if item.sell_in < 11 && item.quality < 50
-                item.quality += 1
-              end
-              if item.sell_in < 6 && item.quality < 50
-                item.quality += 1
-              end
-            end
           end
-        else
-          item.quality -= 1 if item.quality > 0
+          if item.sell_in < 6 && item.quality < 50
+            item.quality += 1
+          end
         end
 
         item.sell_in = item.sell_in - 1
 
+        item.quality = 0 if item.sell_in < 0
+      else
+        item.quality -= 1 if item.quality > 0
+
+        item.sell_in = item.sell_in - 1
+
         if item.sell_in < 0
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            item.quality = 0
-          elsif item.quality > 0
+          if item.quality > 0
             item.quality -= 1
           end
         end
